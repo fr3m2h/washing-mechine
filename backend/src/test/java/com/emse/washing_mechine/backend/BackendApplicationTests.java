1ed36978a5783ab34a5aa0f09b9476e101a572fd
fr3m2h <156535827+fr3m2h@users.noreply.github.com>
2024-12-22 18:07:53 +0100

    Created inital Spring Boot project

---
+++ backend/src/test/java/com/emse/washing_mechine/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emse.washing_mechine.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
