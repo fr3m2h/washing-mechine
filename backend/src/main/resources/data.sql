f191dca3a596d201ceb83f004bce6ce5f52fbc74
Hugo NOEL <hugo_emse@hugoemse>
2025-01-10 22:37:47 +0100

    maj backend

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO SP_MACHINE (id, name, is_used, isHs, time_left) VALUES (-10, "Machine 1", true, false, 12.5);
-INSERT INTO SP_MACHINE (id, name, is_used, isHs, time_left) VALUES (-9, "Machine 2", false, false, 0.0);
-INSERT INTO SP_MACHINE (id, name, is_used, isHs, time_left) VALUES (-8, "Machine 3", false, false, 30.0);
-INSERT INTO SP_MACHINE (id, name, is_used, isHs, time_left) VALUES (-7, "Machine 4", false, false, 8.0);
-INSERT INTO SP_MACHINE (id, name, is_used, isHs, time_left) VALUES (-6, "Machine 5", false, false, 4.0);
-INSERT INTO SP_MACHINE (id, name, is_used, isHs, time_left) VALUES (-5, "Machine 6", false, false, 0.0);
-INSERT INTO SP_MACHINE (id, name, is_used, isHs, time_left) VALUES (-4, "Machine 7", true, false, 18.5);
+INSERT INTO SP_MACHINE (id, name, is_used, is_hs, time_left) VALUES (-10, "Machine 1", true, false, 12.5);
+INSERT INTO SP_MACHINE (id, name, is_used, is_hs, time_left) VALUES (-9, "Machine 2", false, false, 0.0);
+INSERT INTO SP_MACHINE (id, name, is_used, is_hs, time_left) VALUES (-8, "Machine 3", false, false, 30.0);
+INSERT INTO SP_MACHINE (id, name, is_used, is_hs, time_left) VALUES (-7, "Machine 4", false, false, 8.0);
+INSERT INTO SP_MACHINE (id, name, is_used, is_hs, time_left) VALUES (-6, "Machine 5", false, false, 4.0);
+INSERT INTO SP_MACHINE (id, name, is_used, is_hs, time_left) VALUES (-5, "Machine 6", false, false, 0.0);
+INSERT INTO SP_MACHINE (id, name, is_used, is_hs, time_left) VALUES (-4, "Machine 7", true, false, 18.5);
